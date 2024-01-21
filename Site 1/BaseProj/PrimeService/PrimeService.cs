85667c779906b421d75c3799b54d2275ecd89806
thegman254 <gabrielplayroom@gmail.com>
2024-01-21 09:11:43 +0000

    Prime service added

---
+++ Site 1/BaseProj/PrimeService/PrimeService.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Prime.Services
+{
+    public class PrimeService
+    {
+        public class PrimeService
+    {
+        public bool IsPrime(int candidate)
+        {
+            throw new NotImplementedException("Please create a test first.");
+        }
+    }
+    }
+}
