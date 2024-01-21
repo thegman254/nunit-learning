9755e4de33990c08d28da383214b3fbbc4292860
thegman254 <gabrielplayroom@gmail.com>
2024-01-21 09:56:46 +0000

    Redid setup

---
+++ WTRFolder/NUnitTestProject/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace NUnitTestProject;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
---
+++ WTRFolder/WebTrainingRoom/WebTrainingRoom.cs
@@ -0,0 +1,10 @@
+﻿namespace WebTrainingRoom;
+
+public class Calculator
+{
+    public int Add(int a, int b)
+    {
+        int x = a + b;
+        return x;
+    }
+}
